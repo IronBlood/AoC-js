9659f015b87e540eea0edb68ea696c8bb1f69c28
IronBlood <aplshenyang@gmail.com>
2025-09-01 17:33:13 +0800

    refactor: 2018 import jest symbols for LSP

--- src/2018/day-23/lib.test.js
+++ src/2018/day-23/lib.test.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from '@jest/globals';
 import {
 	total_number,
 	shortest_distance,
--- src/2018/day-24/lib.test.js
+++ src/2018/day-24/lib.test.js
@@ -1,3 +1,4 @@
+import { describe, it, expect } from '@jest/globals';
 import {
 	parse_groups,
 	winning_units,
