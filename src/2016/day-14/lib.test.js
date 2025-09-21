eda9a2a30b0407f7e10b3b3a6289f9ef6e5ae274
IronBlood <aplshenyang@gmail.com>
2025-09-21 11:38:33 +0800

    use env `SKIP_MD5` to skip unit tests

--- src/2016/day-14/lib.test.js
+++ src/2016/day-14/lib.test.js
@@ -3,6 +3,8 @@ import {
 	get_index,
 } from "./lib.js";
 
+const should_skip = !!process.env.SKIP_MD5;
+
 describe("2016-12-14 p1", () => {
 	const testcases = [
 		[`abc`, 22728],
@@ -16,7 +18,7 @@ describe("2016-12-14 p1", () => {
 	}
 });
 
-describe("2016-12-14 p2", () => {
+(should_skip ? describe.skip : describe)("2016-12-14 p2", () => {
 	const testcases = [
 		[`abc`, 22551],
 	];
